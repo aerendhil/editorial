d53569c7d45ef037870c3b43568ca73f97e54ca6
Pablo <pablorozasb@gmail.com>
2019-10-17 19:48:13 -0300

    Rama nueva con avances

--- appRegistro/models.py
+++ appRegistro/models.py
@@ -3,5 +3,12 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
 	run = models.CharField(max_length=20)
-	nombres = models.CharField(max_length=50)
-	apellidos = models.CharField(max_length=50)
+	nombres = models.CharField(max_length=50, default = "hola")
+	apellidos = models.CharField(max_length=50, default = "hola")
+	correo = models.CharField(max_length=50, default = "hola")
+	#nacimiento = #DATE
+	telefono = models.CharField(max_length=50, default = "hola")
+	region = models.CharField(max_length=50, default = "hola")
+	ciudad = models.CharField(max_length=50, default = "hola")
+	comuna = models.CharField(max_length=50, default = "hola")
+	vivienda = models.CharField(max_length=50, default = "hola")
--- appRegistro/views.py
+++ appRegistro/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def registro(request):
+	if request.method == "POST":
+		form = CarreraForm(request.POST)
+		if form.is_valid():
+			model_instance = form.save(commit = false)
+			model_instance.save()
+			return redirect('registo_completado')
