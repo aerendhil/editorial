66764c29c3ab3321b7e3bb8878720111285e8918
aerendil <arielastudillo@outlook.es>
2019-10-27 17:37:39 -0300

    Se agrego livereload para instalacion al archivo get_django.bat

--- get_django.bat
+++ get_django.bat
@@ -1,4 +1,4 @@
 python -m pip list
 python -m pip install --upgrade pip
-python -m pip install autopep8 django
+python -m pip install autopep8 django django-livereload-server
 python manage.py migrate
