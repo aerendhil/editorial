d762c53645b25828bdbbe32989318217edf6b128
Pablo <pablorozasb@gmail.com>
2019-10-28 00:20:13 -0300

    Prototipo de registro

--- appRegistro/views.py
+++ appRegistro/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
+from .forms import UsuarioForm
+from .models import Usuario
+from appEditorial import templates
 
 # Create your views here.
 def registro(request):
 	if request.method == "POST":
-		form = CarreraForm(request.POST)
+		form = UsuarioForm(request.POST)
 		if form.is_valid():
-			model_instance = form.save(commit = false)
+			model_instance = form.save(commit = False)
 			model_instance.save()
-			return redirect('registo_completado')
+			return redirect('/editorial')
+	else:
+		form = UsuarioForm()
+		return render(request,
+			"appEditorial/registro.html", {'form': form})
