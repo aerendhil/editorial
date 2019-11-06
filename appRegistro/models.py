86e7cb16b5531da4c924967f384fadc43cfd6550
Pablo <pablorozasb@gmail.com>
2019-11-05 22:04:36 -0300

    cambios varios

--- appRegistro/models.py
+++ appRegistro/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Usuario(models.Model):
-	run = models.CharField(max_length=20)
-	nombres = models.CharField(max_length=50, default = "hola")
-	apellidos = models.CharField(max_length=50, default = "hola")
-	correo = models.CharField(max_length=50, default = "hola")
-	#nacimiento = #DATE
-	telefono = models.CharField(max_length=50, default = "hola")
-	region = models.CharField(max_length=50, default = "hola")
-	ciudad = models.CharField(max_length=50, default = "hola")
-	comuna = models.CharField(max_length=50, default = "hola")
-	vivienda = models.CharField(max_length=50, default = "hola")
+	run = models.CharField(max_length=20, default = "no_asignado")
+	nombres = models.CharField(max_length=50, default = "no_asignado")
+	apellidos = models.CharField(max_length=50, default = "no_asignado")
+	correo = models.EmailField(max_length=254, default = "no_asignado")
+	nacimiento = models.DateField(auto_now=False, auto_now_add=False, default= datetime.date(1997, 10, 19))
+	telefono = models.CharField(max_length=50, default = "no_asignado")
+	region = models.CharField(max_length=50, default = "no_asignado")
+	ciudad = models.CharField(max_length=50, default = "no_asignado")
+	comuna = models.CharField(max_length=50, default = "no_asignado")
+	vivienda = models.CharField(max_length=50, default = "no_asignado")
