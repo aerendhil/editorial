6018f7968f26a8ae138f8b80a6fbea169809fd4c
Pablo <pablorozasb@gmail.com>
2019-11-18 15:23:43 -0300

    asdf

--- appEditorial/forms.py
+++ appEditorial/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 from .models import Contacto, Autor, Editorial, Libro
 
 class ContactoForm(ModelForm):
--- sicmundus/settings.py
+++ sicmundus/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appRegistro',
     'appEditorial',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
