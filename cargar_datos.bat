47631f3cfb9c44925e12f3b47b9b81f8aa890d1f
Pablo <pablorozasb@gmail.com>
2019-11-19 02:07:05 -0300

    orasi

--- cargar_datos.bat
+++ cargar_datos.bat
@@ -1,3 +1,4 @@
+REM Para cargar los datos por defecto: correr este script dentro del ambiente
 python manage.py loaddata appEditorial/fixtures/autores.json
 python manage.py loaddata appEditorial/fixtures/editoriales.json
 python manage.py loaddata appEditorial/fixtures/libros.json
--- get_apps.bat
+++ get_apps.bat
@@ -1,3 +1,4 @@
+REM Correr este script dentro del ambiente
 python -m pip list
 python -m pip install --upgrade pip
 python -m pip install autopep8 django django-livereload-server django-extensions pydotplus pillow django-crispy-forms django-filter
