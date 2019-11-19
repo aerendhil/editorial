9f2749b5308baf8ada093a9e10f278b8b3b2fb43
Pablo <pablorozasb@gmail.com>
2019-11-19 02:12:04 -0300

    correciones finales

--- get_apps.bat
+++ get_apps.bat
@@ -3,3 +3,4 @@ python -m pip list
 python -m pip install --upgrade pip
 python -m pip install autopep8 django django-livereload-server django-extensions pydotplus pillow django-crispy-forms django-filter
 python manage.py makemigrations
+python manage.py migrate
