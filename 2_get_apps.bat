b9dce1251c139af06e5f1cdae8385dfc0e2f762b
Pablo <pablorozasb@gmail.com>
2019-12-03 17:36:53 -0300

    agregado djangorestframework

--- 2_get_apps.bat
+++ 2_get_apps.bat
@@ -3,6 +3,6 @@ REM Este script carga todas las aplicaciones y general la bd
 
 python -m pip list
 python -m pip install --upgrade pip
-python -m pip install autopep8 django==2.2 django-livereload-server django-extensions pydotplus pillow django-crispy-forms django-filter
+python -m pip install autopep8 django==2.2 django-livereload-server django-extensions pydotplus pillow django-crispy-forms django-filter djangorestframework==3.10
 python manage.py makemigrations
 python manage.py migrate
