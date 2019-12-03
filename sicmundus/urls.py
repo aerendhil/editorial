d98b467029eeb11df25e9834c6e4d1b45a58a0e1
Pablo <pablorozasb@gmail.com>
2019-12-03 20:55:17 -0300

    correción del error de contraseña

--- sicmundus/urls.py
+++ sicmundus/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('appEditorial.urls')),
     path('admin/', admin.site.urls),
     path('ad/', include('appRegistro.urls')),
+    url(r'', include('django.contrib.auth.urls')),
 ]
